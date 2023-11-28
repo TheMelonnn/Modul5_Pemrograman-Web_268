cae35f880b2785aa845d95ed3179724c956a3168
TheMelonnn <zidanrifardi125@gmail.com>
2023-11-28 15:57:37 +0700

    Push Codelab Modul 5

---
+++ app/Config/DatabaseConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\config;
+
+class DatabaseConfig
+{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "modul_5_web";
+    public $port = 3306;
+}
